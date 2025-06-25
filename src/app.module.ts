3736fc0220d8905a2ac4e427f99381d21ac79925
Alonso <abrahamreynoso64@gmail.com>
2025-06-25 16:04:06 -0600

    feat: eliminando variables de ejemplo e implementando .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,23 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserLockerModule } from './user-lockers/user-locker.module';
 import { UserCompartmentModule } from './user-compartments/user-compartment.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'token1234',
-      database: 'lockity_db',
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false,
     }),
     UserLockerModule,
-    UserCompartmentModule
+    UserCompartmentModule,
+    ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
