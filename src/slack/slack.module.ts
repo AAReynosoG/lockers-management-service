b5851935cfe5a0c9a19727da500ed671a7d67c50
Alonso <abrahamreynoso64@gmail.com>
2025-06-28 16:57:33 -0600

    refactor: implementando envio de emails

--- src/slack/slack.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SlackService } from './slack.service';
-
-@Module({
-  imports: [],
-  controllers: [],
-  providers: [SlackService],
-})
-export class SlackModule {}
