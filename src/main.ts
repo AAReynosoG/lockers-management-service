c3c06aeb16f510dad61f6f13af9513c0525018a4
Alonso <abrahamreynoso64@gmail.com>
2025-06-28 23:40:26 -0600

    refactor: incluyendo CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { SlackService } from './communication/slack/slack.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
