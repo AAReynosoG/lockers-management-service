8b9ec300ab74d8e252d4b081ee17118e00efa2d0
Alonso <abrahamreynoso64@gmail.com>
2025-08-01 23:02:51 -0600

    refactor: volviendo a la configuración inicial para la conexión de MongoDB

--- config/mongo.ts
+++ config/mongo.ts
@@ -1,7 +1,20 @@
 import env from '#start/env'
-import { MongoClient } from 'mongodb'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
 
-const mongoClient = new MongoClient(env.get('MONGODB_DG_VPC_URI')!, {
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const credentials = join(__dirname, 'mongo-certs', 'X509-cert-1532914571237800098.pem')
+
+const mongoClient = new MongoClient(env.get('MONGODB_URI_SSL')!, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  tlsCertificateKeyFile: credentials,
   maxPoolSize: 10,
 })
 
