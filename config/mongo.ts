6d49031b943f2a6ae403ade712c3e3236318f57e
Alonso <abrahamreynoso64@gmail.com>
2025-08-01 22:39:43 -0600

    refactor: implementando conexión por la VPC de DG

--- config/mongo.ts
+++ config/mongo.ts
@@ -1,20 +1,7 @@
 import env from '#start/env'
-import { MongoClient, ServerApiVersion } from 'mongodb'
-import { fileURLToPath } from 'url'
-import { join, dirname } from 'path'
+import { MongoClient } from 'mongodb'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const credentials = join(__dirname, 'mongo-certs', 'X509-cert-1532914571237800098.pem')
-
-const mongoClient = new MongoClient(env.get('MONGODB_URI_SSL')!, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-  tlsCertificateKeyFile: credentials,
+const mongoClient = new MongoClient(env.get('MONGODB_DG_VPC_URI')!, {
   maxPoolSize: 10,
 })
 
